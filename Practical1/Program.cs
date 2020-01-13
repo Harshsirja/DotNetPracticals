2a703bc3b8453784aa665e8f7278df5db94df1dc
Harsh Sirja <17comp.haarh.sirja@gmail.com>
2020-01-13 13:45:23 +0530

    Initial Commit

---
+++ Practical1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            pattern1(); //This will print Pattern_1
+            Console.WriteLine();
+            pattern2(); //This will print Pattern_2
+            Console.WriteLine();
+            pattern3(); //This will print Pattern_3
+        }
+        static void pattern1()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void pattern2()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = i; j < 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void pattern3()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
